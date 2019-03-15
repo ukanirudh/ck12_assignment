123d10b0060dc030defca79416f68222bd9a7e24
U K ANIRUDH <anirudh.anirudh93@gmail.com>
2019-03-15 18:39:06 +0530

    Anirudh : Adding proxy and initial api call

--- public/assets/js/components/App.js
+++ public/assets/js/components/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  render() {
+
+  componentWillMount () {
+    this.setState({books: []})
+  }
+
+  componentDidMount () {
+    fetch('http://localhost:3000/api/book/maths')
+    .then(response => response.json())
+    .then(data => this.setState({books: data}))
+    .catch(error => console.log(error))
+  }
+
+  render () {
+    const {books} = this.state
     return (
-      <h2>Test server</h2>
+      books.map((book) => {
+        const {title, id} = book
+        return (<h2>{title}</h2>)
+      })
     );
   }
 }
